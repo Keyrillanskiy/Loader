1d096e96d9a58b9788094694916ae9b40a2ca2b2
keyrillanskiy <keyrillanskiy@gmail.com>
2021-11-11 19:55:10 +0300

    added main activity, added layouts for screen and list items, added colors for text and decoration

---
+++ app/src/main/java/com/github/keyrillanskiy/loader/presentation/screens/LoaderActivity.kt
@@ -0,0 +1,14 @@
+package com.github.keyrillanskiy.loader.presentation.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.keyrillanskiy.loader.R
+
+class LoaderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loader)
+    }
+
+}
